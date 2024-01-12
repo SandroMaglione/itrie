bf781340c9d06766ecd9c8920a377079f64c0890
SandroMaglione <lass.maglio@gmail.com>
2024-01-12 05:11:43 +0100

    example with `4.64 MB` file words

--- example/itrie_example.dart
+++ example/itrie_example.dart
@@ -1,1 +1,36 @@
-void main() {}
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:itrie/itrie.dart';
+
+void main() async {
+  /// Source: https://github.com/dwyl/english-words/blob/master/words.txt
+  final file = File('./example/words.txt');
+  Stream<String> lines = file.openRead().transform(utf8.decoder).transform(
+        LineSplitter(),
+      );
+
+  try {
+    final itrie = ITrie<int>.fromIterable(
+      await lines
+          .map(
+            (key) => (key.toLowerCase(), key.length),
+          )
+          .toList(),
+    );
+
+    while (true) {
+      print("Search a word\n");
+      final line = stdin.readLineSync(encoding: utf8);
+      if (line != null) {
+        if (line == "q") {
+          break;
+        }
+
+        print(itrie.get(line));
+      }
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
